8d42a1e42720fc5057eb5cf7095b3cb7831e0228
seanthenderson <seanthenderson@gmail.com>
2018-01-23 21:33:13 -0500

    Initial commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Media from 'react-media'
+
+const Sidebar = (props) => (
+<div
+    style={{
+      border: '2px solid #e6e6e6',
+      maxWidth: 960,
+      padding: '0.5rem',
+      marginBottom: '25px'
+    }}
+    >
+    <strong>{props.title}.</strong> {props.description}
+</div>
+);
+
+export default Sidebar;
